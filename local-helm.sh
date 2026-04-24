60cd5eebae0b3232ba7467511d8e92d6911eb61e
Denys Ponomarenko <42810222+OxiDis2004@users.noreply.github.com>
2026-04-24 12:13:32 +0200

    update initialize script

--- local-helm.sh
+++ local-helm.sh
@@ -8,19 +8,24 @@ export GITHUB_USERNAME=$(grep -E '^github_username=' ~/.nutri-system.properties 
 export GITHUB_PASSWORD=$(grep -E '^github_password=' ~/.nutri-system.properties | cut -d= -f2- | tr -d '\r')
 export TELEGRAM_TOKEN=$(grep -E '^telegram_token=' ~/.nutri-system.properties | cut -d= -f2- | tr -d '\r')
 
-#helm -n $NAMESPACE uninstall $RELEASE
-#kubectl delete secret telegram-secret ghcr-secret -n $NAMESPACE
-#kubectl delete pvc mysql-pvc -n $NAMESPACE
+helm uninstall $RELEASE -n $NAMESPACE
+kubectl delete pvc mysql-pvc -n $NAMESPACE
+
+kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl create secret generic telegram-secret \
   --from-literal=token=$TELEGRAM_TOKEN \
-  -n $NAMESPACE
+  -n $NAMESPACE \
+  --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl create secret docker-registry ghcr-secret \
   --docker-server=ghcr.io \
   --docker-username=$GITHUB_USERNAME \
   --docker-password=$GITHUB_PASSWORD \
-  -n $NAMESPACE
+  -n $NAMESPACE \
+  --dry-run=client -o yaml | kubectl apply -f -
 
-helm upgrade -i --namespace $NAMESPACE --create-namespace $RELEASE ./chart --values ./chart/values.yaml
+helm upgrade -i $RELEASE ./chart \
+  --namespace $NAMESPACE \
+  --create-namespace
 kubectl config set-context --current --namespace=$NAMESPACE
