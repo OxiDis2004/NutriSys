7898037e6de8dc3173f39d6d579f5c1f300c5845
Denys Ponomarenko <42810222+OxiDis2004@users.noreply.github.com>
2026-05-17 13:29:43 +0200

    update ci/cd

--- local-helm.sh
+++ local-helm.sh
@@ -4,8 +4,11 @@ set -euo pipefail
 
 export RELEASE=nutri-sys-ns
 export NAMESPACE=stack
+# shellcheck disable=SC2155
 export GITHUB_USERNAME=$(grep -E '^github_username=' ~/.nutri-system.properties | cut -d= -f2- | tr -d '\r')
+# shellcheck disable=SC2155
 export GITHUB_PASSWORD=$(grep -E '^github_password=' ~/.nutri-system.properties | cut -d= -f2- | tr -d '\r')
+# shellcheck disable=SC2155
 export TELEGRAM_TOKEN=$(grep -E '^telegram_token=' ~/.nutri-system.properties | cut -d= -f2- | tr -d '\r')
 
 helm uninstall $RELEASE -n $NAMESPACE
@@ -14,14 +17,14 @@ kubectl delete pvc mysql-pvc -n $NAMESPACE
 kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl create secret generic telegram-secret \
-  --from-literal=token=$TELEGRAM_TOKEN \
+  --from-literal=token="$TELEGRAM_TOKEN" \
   -n $NAMESPACE \
   --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl create secret docker-registry ghcr-secret \
   --docker-server=ghcr.io \
-  --docker-username=$GITHUB_USERNAME \
-  --docker-password=$GITHUB_PASSWORD \
+  --docker-username="$GITHUB_USERNAME" \
+  --docker-password="$GITHUB_PASSWORD" \
   -n $NAMESPACE \
   --dry-run=client -o yaml | kubectl apply -f -
 
